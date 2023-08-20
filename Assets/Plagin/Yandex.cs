3413322f19aa3498d37a8c744c645ecf6a50d4c7
Aibulat <Myyfeedi@gmail.com>
2023-08-20 15:59:08 +0600

    save

---
+++ Assets/Plagin/Yandex.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Yandex : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void ShowReclama();
+    public void ShowReclamaInUnity()
+    {
+#if UNITY_EDITOR
+        Debug.Log("unity");
+#else
+#if UNITY_WEBGL
+        Debug.Log("webgl");
+        ShowReclama();
+#endif
+#endif
+    }
+}
