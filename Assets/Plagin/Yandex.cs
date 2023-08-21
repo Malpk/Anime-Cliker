bbed4df1fe85354587d77905977d26dceb1019f2
Malpk <byabin.max@yandex.ru>
2023-08-21 12:58:55 +0700

    Fix_save_and_connect_ads
    -поправил сохранения
    -подключил рекламу

--- Assets/Plagin/Yandex.cs
+++ Assets/Plagin/Yandex.cs
@@ -3,17 +3,36 @@ using UnityEngine;
 
 public class Yandex : MonoBehaviour
 {
+    [Min(1)]
+    [SerializeField] private int _adsShowDelay;
+
+    private float _progress = 0;
+
     [DllImport("__Internal")]
-    private static extern void ShowReclama(); 
+    private static extern void ShowReclama();
+
+    private void Reset()
+    {
+        _adsShowDelay = 3;
+    }
+
+    public void ShowAds()
+    {
+        _progress++;
+        if (_progress >= _adsShowDelay)
+        {
+            _progress = 0;
+            ShowReclamaInUnity();
+        }
+    }
+
     public void ShowReclamaInUnity()
     {
 #if UNITY_EDITOR
-        Debug.Log("unity");
-#else
-#if UNITY_WEBGL
+        Debug.Log("showAds_editor");
+#elif UNITY_WEBGL
         Debug.Log("webgl");
         ShowReclama();
 #endif
-#endif
-    } 
+    }
 }
--- Assets/Script/Data/DataSaver.cs
+++ Assets/Script/Data/DataSaver.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class DataSaver : MonoBehaviour
 {
+    [Min(1)]
+    [SerializeField] private float _saveDelay;
     [SerializeField] private string _key;
     [Header("Reference")]
     [SerializeField] private GirlSwitcher _girlSwitcher;
     [SerializeField] private GirlCollectionMenu _collectionGirl;
 
+    private void Reset()
+    {
+        _saveDelay = 3;
+    }
+
     private void Start()
     {
         Load();
+        StartCoroutine(SaveUpdate());
     }
 
     private void OnApplicationQuit()
@@ -35,4 +44,15 @@ public class DataSaver : MonoBehaviour
             _collectionGirl.Load(data.OpenGirlMenu);
         }
     }
+
+    private IEnumerator SaveUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_saveDelay);
+            Save();
+            yield return null;
+        }
+    }
+
 }
