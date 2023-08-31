c13a2b848b73bc63976ca1797640fd9e124bc6ef
Malpk <byabin.max@yandex.ru>
2023-08-31 19:27:25 +0700

    Ads_add_timer

--- Assets/Plagin/Yandex.cs
+++ Assets/Plagin/Yandex.cs
@@ -1,28 +1,39 @@
 using System.Runtime.InteropServices;
+using System.Collections;
 using UnityEngine;
 
 public class Yandex : MonoBehaviour
 {
     [Min(1)]
-    [SerializeField] private int _adsShowDelay;
+    [SerializeField] private int _adsSteep;
+    [Min(1)]
+    [SerializeField] private int _adsDelayShow;
+    [Header("Reference")]
+    [SerializeField] private TextUI _text;
 
     private float _progress = 0;
+
+    private Coroutine _ads;
 
     [DllImport("__Internal")]
     private static extern void ShowReclama();
 
     private void Reset()
     {
-        _adsShowDelay = 3;
+        _adsSteep = 3;
+        _adsDelayShow = 3;
     }
 
     public void ShowAds()
     {
-        _progress++;
-        if (_progress >= _adsShowDelay)
+        if (_ads == null)
         {
-            _progress = 0;
-            ShowReclamaInUnity();
+            _progress++;
+            if (_progress >= _adsSteep)
+            {
+                _progress = 0;
+                _ads = StartCoroutine(ShowWaitAds(_adsDelayShow));
+            }
         }
     }
 
@@ -35,4 +46,21 @@ public class Yandex : MonoBehaviour
         ShowReclama();
 #endif
     }
+
+    private IEnumerator ShowWaitAds(int delay)
+    {
+        var progress = 0f;
+        _text.gameObject.SetActive(true);
+        while (progress < 1f)
+        {
+            progress += Time.deltaTime / delay;
+            var value = delay * (1 - progress);
+            _text.SetText((int)value);
+            yield return null;
+        }
+        _text.gameObject.SetActive(false);
+        ShowReclamaInUnity();
+        _ads = null;
+    }
+
 }
